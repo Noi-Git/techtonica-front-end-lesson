136a00691f85c9a3b55cf1e9060dbacc567cf53d
Noi <noi.patsin@gmail.com>
2019-05-20 18:40:13 -0700

    basic javascript

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,15 @@
 // Your Javascript will go here!
+
+// $("#copyright").click(function() {
+//     $(this).css('color', 'purple');
+//     $(this).css('background-color', 'pink');
+// })
+
+// document.getElementById('copyright').addEventListener("click", function() {
+//     this.setAttribute("style", "color : purple; background-color : pink;")
+// })
+
+document.getElementById('copyright').addEventListener("click", function() {
+    this.style.color = 'purple';
+})
+
